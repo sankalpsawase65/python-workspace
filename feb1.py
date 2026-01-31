630f6d19c890b8d1a292101b5de6b3813a21f697
sankalpsawase65 <sankalpsawase65@gmail.com>
2026-01-31 21:28:17 +0530

    Remove unused code for finding sorted order of marks

--- feb1.py
+++ feb1.py
@@ -11,15 +11,11 @@ name3 = input("Enter name 3: ")
 roll3 = eval(input("Enter roll number 3: "))
 marks3 = eval(input("Enter marks 3: "))
 
-# Finding sorted order of marks
-low = min(marks1, marks2, marks3)
-high = max(marks1, marks2, marks3)
-mid = marks1 + marks2 + marks3 - low - high
-
 # Printing table
 print("{:<15}{:<15}{:<10}".format("Name", "Roll No", "Marks"))
 print("-" * 40)
 print("{:<15}{:<15}{:<10}".format(name1, roll1, marks1))
 print("{:<15}{:<15}{:<10}".format(name2, roll2, marks2))
 print("{:<15}{:<15}{:<10}".format(name3, roll3, marks3))
+
 
